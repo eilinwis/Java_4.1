f1ff355e12c1c5f5c2558910d45df580fea1bf8b
Roman Kudryavtsev <TTe4kiH98@gmail.com>
2021-03-15 12:56:16 +0300

    Code edited

---
+++ src/BonusMilesService.java
@@ -0,0 +1,9 @@
+public class BonusMilesService {
+    public int calculate(int cost) {
+        int stone = 20;
+        int miles = cost / stone;
+       // System.out.println(miles);
+
+        return miles;
+    }
+}
